d279730ea4335fa8ae5f33c82b88fc4fcfe63a4a
Andrés Núñez <nunezlopez.ja@gmail.com>
2026-08-22 16:16:58 -0600

    Added first template. Improved HomeHandler

--- main.go
+++ main.go
@@ -1,34 +1,45 @@
 package main
 
 import (
-	"fmt"
+	"html/template"
+	"log"
 	"net/http"
 )
 
 type Vehicle struct {
-	Plate      string
-	Maker      string
-	Model      string
-	Year       string
-	AssignedTo string
+	ID          int
+	Maker       string
+	Model       string
+	Year        string
+	Plate       string
+	LastService string
+	AssignedTo  string
+	Location    string
 }
 
 var vehicles = []Vehicle{
-	{"ABC-123", "Chevrolet", "Spark", "2017", "Andrés"},
-	{"DEF-456", "Chevrolet", "Chevy", "2010", "Miguel"},
+	{1, "Chevrolet", "Spark", "2018", "ABC-123", "21-08-2026", "Abraham", "Morelia"},
+	{2, "Chevrolet", "Chevy", "2010", "DEF-456", "05-06-2026", "Miguel", "Pátzcuaro"},
+	{3, "Hyndai", "Atos", "2011", "GHI-789", "31-07-2026", "Fulano", "Vallarta"},
+	{4, "Ford", "Icon", "2015", "JKL-123", "30-05-2026", "Hermilo", "Morelia"},
 }
 
-func hello(w http.ResponseWriter, req *http.Request) {
-	fmt.Fprintf(w, "Hola, CIETEL!\n")
-
-	for _, v := range vehicles {
-		fmt.Fprintf(w, "Vehicle: %s\t%s\t%s\t%s\n", v.Maker, v.Model, v.Year, v.Plate)
+func HomeHandler(w http.ResponseWriter, req *http.Request) {
+	tmpl, err := template.ParseFiles("templates/index.html")
+	if err != nil {
+		log.Fatal(err)
 	}
-	fmt.Println("Replacing on template...")
+
+	// for _, v := range vehicles {
+	// 	fmt.Println("Vehicle: %s\t%s\t%s\t%s\n", v.Maker, v.Model, v.Year, v.Plate)
+	// }
+	err = tmpl.Execute(w, vehicles)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
 
 func main() {
-	http.HandleFunc("/vehiculos", hello)
-
+	http.HandleFunc("/vehiculos", HomeHandler)
 	http.ListenAndServe(":8080", nil)
 }

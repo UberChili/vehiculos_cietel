c43ac5a3e11d17147f0c4ed868e532f16ceda6e2
Andrés Núñez <nunezlopez.ja@gmail.com>
2026-08-21 21:15:47 -0600

    Initial commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/UberChili/vehiculos_cietel
+
+go 1.25.0
---
+++ main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+type Vehicle struct {
+	Plate      string
+	Maker      string
+	Model      string
+	Year       string
+	AssignedTo string
+}
+
+var vehicles = []Vehicle{
+	{"ABC-123", "Chevrolet", "Spark", "2017", "Andrés"},
+	{"DEF-456", "Chevrolet", "Chevy", "2010", "Miguel"},
+}
+
+func hello(w http.ResponseWriter, req *http.Request) {
+	fmt.Fprintf(w, "Hola, CIETEL!\n")
+
+	for _, v := range vehicles {
+		fmt.Fprintf(w, "Vehicle: %s\t%s\t%s\t%s\n", v.Maker, v.Model, v.Year, v.Plate)
+	}
+
+	fmt.Println("Replacing on template...")
+}
+
+func main() {
+	http.HandleFunc("/vehiculos", hello)
+
+	http.ListenAndServe(":8080", nil)
+}

c90740fe710dc67a7b2d80a7dd9e6e47b435ef54
Andrés Núñez <nunezlopez.ja@gmail.com>
2026-09-08 11:08:58 -0600

    Starting from almost zero. Initial commit

--- go.mod
+++ go.mod
@@ -1,7 +1,5 @@
-module github.com/UberChili/vehiculos_cietel
+module github.com/UberChili/vehiculos_cietel_rewrite
 
-go 1.25.0
-
-require github.com/mattn/go-sqlite3 v1.14.50
+go 1.26.7
 
 require github.com/go-chi/chi/v5 v5.3.2 // indirect
--- main.go
+++ main.go
@@ -1,78 +1,19 @@
 package main
 
 import (
-	"database/sql"
-	"html/template"
-	"log"
 	"net/http"
 
 	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
 )
 
-type App struct {
-	db   *sql.DB
-	tmpl *template.Template
-}
-
-// NewRouter wires up every route on top of app. Split out from main so
-// tests can exercise the exact same routing table via httptest, instead of
-// duplicating (and risking drift from) the route list.
-func NewRouter(app *App) http.Handler {
-	r := chi.NewRouter()
-	r.Get("/vehiculos", app.HomeHandler)
-	r.Get("/vehiculos/", app.HomeHandler)
-	r.Get("/vehiculos/{id}", app.HomeHandler)
-	r.Get("/vehiculos/{id}/", app.HomeHandler)
-	r.Get("/vehiculos/{id}/nuevo-registro", app.NewRecordHandler)
-	r.Get("/vehiculos/{id}/nuevo-registro/", app.NewRecordHandler)
-	r.Post("/vehiculos/{id}/nuevo-registro/", app.NewRecordHandler)
-	r.Post("/vehiculos/{id}/nuevo-registro", app.NewRecordHandler)
-	r.Get("/vehiculos/{id}/registro/{record_id}", app.RecordHandler)
-	r.Get("/vehiculos/{id}/registro/{record_id}/", app.RecordHandler)
-	r.Post("/vehiculos/{id}/registro/{record_id}/eliminar", app.DeleteRecordHandler)
-	r.Post("/vehiculos/{id}/registro/{record_id}/eliminar/", app.DeleteRecordHandler)
-	r.Get("/vehiculos/{id}/editar", app.EditVehicleHandler)
-	r.Get("/vehiculos/{id}/editar/", app.EditVehicleHandler)
-	r.Post("/vehiculos/{id}/editar", app.EditVehicleHandler)
-	r.Post("/vehiculos/{id}/editar/", app.EditVehicleHandler)
-	r.Get("/vehiculos/new/", app.NewVehicleHandler)
-	r.Get("/vehiculos/new", app.NewVehicleHandler)
-	r.Post("/vehiculos/new/", app.NewVehicleHandler)
-	r.Post("/vehiculos/new", app.NewVehicleHandler)
-	r.Handle("/uploads/*", http.StripPrefix("/uploads/", http.FileServer(http.Dir("uploads"))))
-
-	return r
-}
-
-// ParseTemplates loads every page template with the shared function map
-// (formatDate) available to all of them.
-func ParseTemplates() (*template.Template, error) {
-	return template.New("").Funcs(template.FuncMap{
-		"formatDate": FormatDateDisplay,
-	}).ParseFiles(
-		"templates/index.html",
-		"templates/vehicle.html",
-		"templates/new_vehicle.html",
-		"templates/edit_vehicle.html",
-		"templates/new_record.html",
-		"templates/record.html",
-		"templates/error.html",
-	)
-}
-
 func main() {
-	templ, err := ParseTemplates()
-	if err != nil {
-		log.Fatal(err)
-	}
+	r := chi.NewRouter()
+	r.Use(middleware.Logger)
 
-	db, err := CreateOrOpenTable("./vehicles.db")
-	if err != nil {
-		log.Fatal("Could not Open or Create table: ", err)
-	}
-	app := App{db, templ}
-	defer app.db.Close()
+	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Welcome!"))
+	})
 
-	r := NewRouter(&app)
-	log.Fatal(http.ListenAndServe("127.0.0.1:8081", r))
+	http.ListenAndServe(":8081", r)
 }
